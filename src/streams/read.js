84730ed0dc397d9e49efc7fc6da0965eff36ef6d
fromarys <fromarys@gmail.com>
2023-02-05 20:11:16 +0600

    add read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const { stdout } = process;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const path = join(__dirname, 'files', 'fileToRead.txt');
+    const content = createReadStream(path, 'utf-8');
+    let data = '';
+    content.on('data', chunk => data += chunk);
+    content.on('end', () => stdout.write(data));
 };
 
 await read();
