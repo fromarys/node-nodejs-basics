8a69cede6db6a62749e44b035ec69dfdd026237b
fromarys <fromarys@gmail.com>
2023-02-05 18:15:55 +0600

    add rename files function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
-const rename = async () => {
-    // Write your code here 
+import { rename } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const renameFile = async () => {
+   const wrongFile = join(__dirname, 'files', 'wrongFilename.txt');
+   const correctFile = join(__dirname, 'files', 'wrongFilename.txt');
+   await rename(wrongFile, correctFile);
 };
 
-await rename();
+await renameFile();
