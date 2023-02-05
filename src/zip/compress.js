f846ad467f9d49b64371191b383e116244efd299
fromarys <fromarys@gmail.com>
2023-02-06 00:55:04 +0600

    add file compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const inputFile = join(__dirname, 'files', 'fileToCompress.txt');
+    const outputFile = join(__dirname, 'files', 'archive.gz');
+    const input = createReadStream(inputFile, 'utf-8');
+    const output = createWriteStream(outputFile);
+    const gzip = zlib.createGzip();
+
+    pipeline(
+        input,
+        gzip,
+        output,
+        err => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
 };
 
 await compress();
