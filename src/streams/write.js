1c3c4f7ea2df93a0d8d8d140e374c3a9b338ebc0
fromarys <fromarys@gmail.com>
2023-02-05 20:21:07 +0600

    add write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const { stdin } = process;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const path = join(__dirname, 'files', 'fileToWrite.txt');
+    const content = createWriteStream(path, 'utf-8');
+    stdin.on('data', (str) => content.write(str, 'utf-8'));
 };
 
 await write();
