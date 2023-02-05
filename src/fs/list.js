51cd54bffb6d2f8ecee7a1ee94e705b87f590691
fromarys <fromarys@gmail.com>
2023-02-05 18:26:09 +0600

    add list function files function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const { stdout } = process;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const folder = join(__dirname, 'files');
+    try {
+        const files = await readdir(folder);
+        stdout.write(files.join('\n'));
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
