1995f279d1312ec018d0deba974c8be01a9a570b
fromarys <fromarys@gmail.com>
2023-02-05 18:30:39 +0600

    add read files function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs/promises';
+import { readFile } from 'fs/promises';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 const create = async () => {
     try {
         const path = join(__dirname, 'files', 'fresh.txt');
-        await writeFile(path, 'I am fresh and young');
+        await readFile(path, 'I am fresh and young');
     }
     catch (err) {
         throw new Error('FS operation failed');
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const { stdout } = process;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const path = join(__dirname, 'files', 'fileToRead.txt');
+        const content = await readFile(path, 'utf-8');
+        stdout.write(content);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
