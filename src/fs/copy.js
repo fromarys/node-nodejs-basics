183e371d9c7360ae1303f19d2b1dea795b813339
fromarys <fromarys@gmail.com>
2023-02-05 18:07:45 +0600

    add create and copy files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { mkdir, readdir, copyFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const source = join(__dirname, 'files');
+    const destination = join(__dirname, 'files_copy');
+    try {
+        await mkdir(destination);
+        const files = await readdir(source);
+        for (let file of files) {
+            const sourceFile = join(source, file);
+            const destinationFile = join(destination, file);
+            copyFile(sourceFile, destinationFile);
+        }
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const path = join(__dirname, 'files', 'fresh.txt');
+        await writeFile(path, 'I am fresh and young');
+    }
+    catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
