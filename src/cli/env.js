f64d3bf2fcd919ad5a834786dd8ba1dd5b16bd84
fromarys <fromarys@gmail.com>
2023-02-05 19:18:05 +0600

    add read env variables function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
+import { env, stdout } from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const prefix='RSS_';
+    const rss = Object.entries(env).filter(x => x[0].startsWith(prefix)).map(x => x.join('='));
+    stdout.write(rss.join('; '));
 };
 
 parseEnv();
