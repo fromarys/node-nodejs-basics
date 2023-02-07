fdb7a9c2a3bba19c950ee27a0bce80dc2deff111
fromarys <fromarys@gmail.com>
2023-02-07 22:00:23 +0600

    add file decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFile = join(__dirname, 'files', 'archive.gz');
+    const outputFile = join(__dirname, 'files', 'fileDecompressed.txt');
+    const input = createReadStream(inputFile);
+    const output = createWriteStream(outputFile);
+    const unzip = zlib.createUnzip();
+
+    pipeline(
+        input,
+        unzip,
+        output,
+        err => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
 };
 
 await decompress();
